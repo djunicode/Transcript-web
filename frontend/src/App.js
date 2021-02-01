52e3e0b13f2a7f172a5ac2e392c86c5c9b881294
Akshath <akshathmahajan13@gmail.com>
2021-02-01 17:42:23 +0530

    Added ProtectedRoute component
    The protected route component redirects a user to /login if they aren't
    logged in.
    Login checks are done by searching for token in redux store instead of
    localStorage as component needs to re-render when token is removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute'
 import store from './redux/store'
 
 function App() {
 	return (
 		<Provider store={store}>
-		<h1>Hello World</h1>
+			<BrowserRouter>
+				<Route exact path="/"> Home page </Route>
+				<Route exact path="/login"> Login page </Route>
+				<ProtectedRoute exact path="/test"> Testing Protected Routes </ProtectedRoute>
+			</BrowserRouter>
 		</Provider>
 	);
 }
