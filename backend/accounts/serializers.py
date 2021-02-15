e046fbeb2ad7a0e87f57f981d39b163620614d12
Akshath <akshathmahajan13@gmail.com>
2021-02-15 17:05:05 +0530

    Added profile creation to user registration
    Added profile creation by extending djoser's UserViewSet and rewriting
    the perform_create function to accommodate generating user profiles
    depending on the type of user.
    Added in StudentProfileSerializer and ManagementProfileSerialzier to
    assist in the same

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,9 +1,8 @@
 from djoser.serializers import UserCreateSerializer
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from django.contrib import auth
-from rest_framework.exceptions import AuthenticationFailed
-import jsonfield
+
+from .models import ManagementProfile, StudentProfile
 
 User = get_user_model()
 
@@ -11,3 +10,14 @@ class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ('id','email','username','is_management','password')
+
+class ManagementProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ManagementProfile
+        fields = ('user', 'staff_id', 'name', 'contact_no')
+
+class StudentProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentProfile
+        fields = ('user', 'sap_id', 'name', 'contact_no', 'department', 'admission_year')
+    
