669f5a0c7c5312a47588e350ecf426e9aca468c0
Akshath <akshathmahajan13@gmail.com>
2021-03-08 10:48:25 +0530

    Catch all route introduced in app.js
    Since we have to keep a catch all 404 page, Template will have to be
    injected at a page level as keeping Template outside routes exempts it
    from the switch.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute'
-import Template from './components/Template'
+// import ProtectedRoute from './components/ProtectedRoute'
+// import Template from './components/Template'
 import store from './redux/store'
 import { CssBaseline, ThemeProvider } from '@material-ui/core'
 import { lightTheme } from './theme/lightTheme'
@@ -15,15 +15,13 @@ function App() {
 			<ThemeProvider theme={lightTheme}>
 				<BrowserRouter>
 					<Switch>
-						{/* Routes outside template */}
 						<Route exact path={URLS.login}> Login page </Route>
-						<Route exact path={URLS.home}> 
-							<ErrorPage code="400" subtitle="Bad Request" text="The server cannot or will not process the request due to something that is perceived to be a client error."/> 
+						<Route exact path={URLS.home}> some home page</Route>
+						<ProtectedRoute exact path="/test"> Testing Protected Routes </ProtectedRoute>
+						{/* Catch all */}
+						<Route path="*" exact>
+							<ErrorPage code="404" subtitle="Not found!" text="The resource you’re looking for doesn’t exist or is temporarily under maintainence." />
 						</Route>
-						<Template>
-							{/* Routes inside template */}
-							<ProtectedRoute exact path="/test"> Testing Protected Routes </ProtectedRoute>
-						</Template>
 					</Switch>
 				</BrowserRouter>
 			</ThemeProvider>
