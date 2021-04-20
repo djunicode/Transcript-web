162c32268d894cfa4b4fc09ea639c898b28c3427
Akshath <akshathmahajan13@gmail.com>
2021-04-20 18:16:41 +0530

    Added permanent login endpoint

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [   
     path('auth/', include('djoser.urls.jwt')),
+    path('auth/login/', PermLoginView.as_view(), name="permanent_token"),
     path('auth/me/', UserInfoApi.as_view(),name='user_info'),
 ]
 
