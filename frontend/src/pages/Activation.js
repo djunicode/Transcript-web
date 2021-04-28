06b0f1f3847fb76a2165b3afc8a538db90117c20
Akshath <akshathmahajan13@gmail.com>
2021-04-28 22:51:47 +0530

    Fixed design of activation page

--- frontend/src/pages/Activation.js
+++ frontend/src/pages/Activation.js
@@ -2,9 +2,18 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Redirect, useParams } from 'react-router'
 import { API_BASE, URLS } from '../consts'
+import PaperContainer from '../components/PaperContainer'
+import { makeStyles, Typography } from '@material-ui/core'
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles(theme=>({
+        container: {minHeight: '95vh', display: 'flex', justifyContent: 'center', alignItems:'center'},
+    })
+)
 
 function Activation() {
     const { uid, token } = useParams()
+    const classes = useStyles()
     const [state, setState] = useState(0)
     useEffect(()=>{
         axios.post(`${API_BASE}/api/activate/`, {uid, token})
@@ -13,7 +22,20 @@ function Activation() {
     }, [])
     
     return (
-        state===1?<Redirect to={URLS.login} />:state===0?<h1>Loading...</h1>:<h1>Failed</h1>
+        state===1?
+            <Redirect to={URLS.login} />:
+            state===0?
+                <PaperContainer className={classes.container}>
+                    <Typography align="center">
+                        <CircularProgress />
+                        <Typography variant="h4">Loading...</Typography>
+                    </Typography>
+                </PaperContainer>:
+                <PaperContainer className={classes.container}>
+                    <Typography variant="h3" align="center">
+                        Activation failed!
+                    </Typography>
+                </PaperContainer>
     )
 }
 
